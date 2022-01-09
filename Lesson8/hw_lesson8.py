110d0cda689f75dda6844ba4d143476eaa7a85cf
UByian <US_Byian@mail.ru>
2022-01-10 00:10:22 +0300

    Lesson#8

--- Lesson8/hw_lesson8.py
+++ Lesson8/hw_lesson8.py
@@ -12,7 +12,6 @@ class Data:
     @classmethod
     def extract(cls, dd_mm_yyyy):
         my_date = []
-
         for i in dd_mm_yyyy.split():
             if i != '-': my_date.append(i)
 
@@ -20,17 +19,12 @@ class Data:
 
     @staticmethod
     def valid(day, month, year):
-
         if 1 <= day <= 31:
             if 1 <= month <= 12:
                 if 2022 >= year >= 0:
-                    return f'All right'
+                    return f'Дата введена корректно'
                 else:
-                    return f'Неправильный год'
-            else:
-                return f'Неправильный месяц'
-        else:
-            return f'Неправильный день'
+                    return f'Неправильная дата'
 
     def __str__(self):
         return f'Текущая дата {Data.extract(self.dd_mm_yyyy)}'
@@ -38,30 +32,164 @@ class Data:
 
 today = Data('06 - 01 - 2022')
 print(today)
-print(Data.valid(11, 11, 2022))
+print(Data.valid(11, 11, 2023))
 print(today.valid(11, 13, 2011))
-print(Data.extract('11 - 11 - 2022'))
-print(today.extract('11 - 11 - 2020'))
-print(Data.valid(1, 11, 2000))
+
 
 # 2. Создайте собственный класс-исключение, обрабатывающий ситуацию деления на нуль.
 # Проверьте его работу на данных, вводимых пользователем.
 # При вводе пользователем нуля в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 
-class DivisionByNull:
+class DivisionNull:
     def __init__(self, divider, denominator):
         self.divider = divider
         self.denominator = denominator
 
     @staticmethod
-    def divide_by_null(divider, denominator):
+    def divide_null(divider, denominator):
         try:
             return (divider / denominator)
         except:
             return (f"Делить на ноль нельзя")
 
 
-div = DivisionByNull(10, 100)
-print(DivisionByNull.divide_by_null(10, 0))
-print(DivisionByNull.divide_by_null(10, 0.1))
-print(div.divide_by_null(100, 0))
+div = DivisionNull(20, 100)
+print(DivisionNull.divide_null(30, 0))
+print(DivisionNull.divide_null(100, 0.1))
+print(div.divide_null(15, 0))
+
+
+# 3. Создайте собственный класс-исключение, который должен проверять содержимое списка на наличие только чисел.
+# Проверить работу исключения на реальном примере.
+# Необходимо запрашивать у пользователя данные и заполнять список только числами.
+# Класс-исключение должен контролировать типы данных элементов списка.
+
+# Примечание: длина списка не фиксирована.
+# Элементы запрашиваются бесконечно, пока пользователь сам не остановит работу скрипта, введя, например, команду “stop”.
+# При этом скрипт завершается, сформированный список с числами выводится на экран.
+# Подсказка: для данного задания примем, что пользователь может вводить только числа и строки.
+# При вводе пользователем очередного элемента необходимо реализовать проверку типа элемента и вносить его в список, только если введено число.
+# Класс-исключение должен не позволить пользователю ввести текст (не число) и отобразить соответствующее сообщение.
+# При этом работа скрипта не должна завершаться.
+
+class MyMethod(Exception):
+    def __init__(self):
+        pass
+
+
+class TypeCheck:
+    def __init__(self):
+        self.my_list = []
+
+    def check_value(self):
+        global user_value
+        while True:
+            try:
+                try:
+                    user_value = int(input('Введите значения: '))
+                    ex = input(f'Значения добавляются "{user_value}" в список. Продолжить? y/n: ').lower()
+                    self.my_list.append(user_value)
+                    if ex == 'n':
+                        print(self.my_list)
+                        break
+                except ValueError:
+                    raise MyMethod
+            except MyMethod:
+                ex = input(f"Введено не число! Продолжить? y/n: ").lower()
+                if ex == 'n':
+                    print(self.my_list)
+                    break
+                else:
+                    self.check_value()
+
+
+a = TypeCheck()
+a.check_value()
+
+
+# 4. Начните работу над проектом «Склад оргтехники».
+# Создайте класс, описывающий склад. А также класс «Оргтехника», который будет базовым для классов-наследников.
+# Эти классы — конкретные типы оргтехники (принтер, сканер, ксерокс).
+# В базовом классе определить параметры, общие для приведенных типов.
+# В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники.
+
+# 5. Продолжить работу над первым заданием.
+# Разработать методы, отвечающие за приём оргтехники на склад и передачу в определенное подразделение компании.
+# Для хранения данных о наименовании и количестве единиц оргтехники,
+# а также других данных, можно использовать любую подходящую структуру, например словарь.
+
+# 6. Продолжить работу над вторым заданием.
+# Реализуйте механизм валидации вводимых пользователем данных.
+# Например, для указания количества принтеров, отправленных на склад, нельзя использовать строковый тип данных.
+
+# Подсказка: постарайтесь по возможности реализовать в проекте «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
+
+class StoreHouseOrg:
+
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.items = {'Модель устройства': self.name, 'Цена за ед': self.price, 'Количество': self.quantity}
+
+    def income(self):
+        try:
+            name = input(f'Введите наименование: ')
+            price = int(input(f'Введите цену за ед: '))
+            quantity = int(input(f'Введите количество: '))
+            item = {'Модель устройства': name, 'Цена за ед': price, 'Количество': quantity}
+            self.items.update(item)
+            print(self.items)
+        except ValueError:
+            print('Ошибка! Некорректное значение!')
+
+
+class Printer(StoreHouseOrg):
+    pass
+
+
+class Scanner(StoreHouseOrg):
+    pass
+
+
+class Notebook(StoreHouseOrg):
+    pass
+
+
+p = Printer('HP LaserJet 2230', 2, 300)
+s = Scanner('Canon', 54000, 10)
+n = Notebook('ASUS', 5, 93000)
+p.income()
+s.income()
+n.income()
+
+
+# 7. Реализовать проект «Операции с комплексными числами».
+# Создайте класс «Комплексное число», реализуйте перегрузку методов сложения и умножения комплексных чисел.
+# Проверьте работу проекта, создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
+# Проверьте корректность полученного результата.
+
+class ComplexNumber:
+    def __init__(self, a, b, *args):
+        self.a = a
+        self.b = b
+
+    def __add__(self, other):
+        print(f'Сумма c1 и c2 равна')
+        return f'{self.a + other.a} + {self.b + other.b} * i'
+
+    def __mul__(self, other):
+        print(f'Произведение c1 и c2 равно')
+        return f'{self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+
+    def __str__(self):
+        return f'{self.a} + {self.b} * i'
+
+
+c1 = ComplexNumber(3, -9)
+c2 = ComplexNumber(5, 7)
+print(c1)
+print(c1 + c2)
+print(c1 * c2)
+
+
